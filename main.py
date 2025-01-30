3cca4cad7318a8ac57f6de58f678458bb0d479ba
IIgor17 <79518940015a@gmail.com>
2025-01-30 18:00:37 +0300

    fastapi post/get template

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "энвилоуп"}
+# Модель запроса
+class UserRequest(BaseModel):
+    id: int
+    query: str
+
+@app.post("/api/request")
+def handle_request(request: UserRequest):
+    """
+    Если query = "1", сервер отвечает "2".
+    В остальных случаях сервер отвечает "3".
+    """
+    if request.query == "ангуляй":
+        response = "энвилоуп"
+    else:
+        response = "не энвилоуп"
+
+    return {
+        "id": request.id,
+        "answer": response
+    }
